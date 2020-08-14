16e06b0f868a8cba1a8cfda8a4a4c8a0105d4f12
neodevelopment <ombuya.nelson.accounts@protonmail.com>
2020-08-14 11:45:30 +0300

    Reduced First Time Setup Time Complexity

--- Includes/Configuration/First Time Setup.php
+++ Includes/Configuration/First Time Setup.php
@@ -1,4 +1,5 @@
 <!--First Time Setup-->
+<!-- A Hilariously Verbose File -->
 <?php
     // Requirements
     require "Configuration.php";    // Has the necessary server details to create a connection
@@ -7,8 +8,8 @@
     $connect = mysqli_connect(server, server_user, server_password);
 
     // These are the tables to be made when first making the database    [Used Multidimensional Associative Array]
-    // Contains both the Table Definition (Schema) and the Data (If Needed)
-    $tables = 
+    // Contains both the Table Definition (Schema) and the Data (If needed)
+    $tables = // Used for Test Data and Default Users
     [
         // Table Name 
         "USERS" =>  array
@@ -65,8 +66,9 @@
         /*
             TO: Create a new DB
             Uses the database name specified in Configuration.php as server_db
-            Creates the DB, then calls createTables() to create it's tables
-            If the database to be made already exists, then check whether it's tables exist using createTables()
+            Creates the DB, then calls checkTables() to create it's tables  
+            NOTE: I don't need to check whether the tables exist if the database doesn't exist, but using checkTables() helps minimize redundancy since I'd still need the first foreach loop
+            If the database to be made already exists, then check whether it's tables exist using checkTables()
         */
         echo "<h2>Database</h2>";
         if ($GLOBALS['connect'] -> query("USE " . server_db) === FALSE)
@@ -83,7 +85,7 @@
                 {
                     echo "<br>Connected to the Database [". server_db ."].<br>";
                     //Since this is a newly created DB, we'll need to make it's tables
-                    createTables();
+                    checkTables();
                 }
                 else
                 {
@@ -101,22 +103,18 @@
         else
         {
             echo "The database [". server_db ."] EXISTS...<br>Checking Tables...";
-            // Function used to check whether tables exist, and make them
-            createTables();
+            // Function used to check whether tables exist
+            checkTables();
         }
         
     }
     
-
-    // Function for checking for and creating tables
-    function createTables()
+    function checkTables()
     {
         /*
-            TO: Makes the tables in the Database
-            The tables to be added can be found in the Associative Array $tables[] on Line 9
-            Syntax for adding the table schema and data can be observed in the $tables[] array
+            TO: Checks whether the tables exist in the Database
 
-            // FIXME: Complexity is O(n^3), Consider Separating Loops
+            NOTE: If the table doesn't exist, it calls the function createTables() which makes the tables
         */
         echo "<br><br><h2>Tables in the [".server_db."] Database.</h2>";
 
@@ -135,6 +133,7 @@
                             LEVEL 3 => Test Data
 
                 Each Foreach loop represents access to one of the levels
+                NOTE: I split the Nested Loops into their own functions in order to save on time complexity
             */
             // Creating the Tables
             foreach ($GLOBALS['tables'] as $table => $options)  //Options being either the SCHEMA or the DATA
@@ -148,62 +147,80 @@
                 }
                 else
                 {
-                    // If the table doesn't exist, then the data also doesn't exist, so we're addiing it
-                    foreach ($options as $option => $queries)   // The SCHEMA Queries or the DATA input Queries
-                    {
-                        // Running through the second level to get the schema
-                        /* 
-                            The 2nd Level in the array is for the SCHEMA, so if it's not an array, it is passed as a query.
-                            And if it is an array, it means it's the DATA array, so it is also taken through a foreach loop.
-                        */
-                        if (is_array($queries) == FALSE)   
-                        {
-                            // If it's not an array, then it's the database schema
-                            echo "The table [$table] doesn't exist...<br>Creating it now...";
-                            $schema_query = $queries;
-
-                            //Creating Table
-                            if ($GLOBALS['connect'] -> query("$schema_query") == TRUE) 
-                            {
-                                echo "<br>Table [$table] has been created successfully.<br>";
-                            }
-                            else
-                            {
-                                echo "<br>Error Making the table [$table]: " . $GLOBALS['connect'] -> connect_error . "<br>";
-                            }
-                        }
-                        else
-                        {
-                            // Checking whether the user wants the test data (NOTE: Check the Configuration.php File)
-                            if ($GLOBALS['add_test_data'] == TRUE || $table === "USERS")    // Added OR to allow for ADMIN to be added even when the flag is OFF
-                            {
-                                // If it is an array, it should go deeper to add the data to the table
-                                echo "<br><u>Adding Data to [$table]...</u><br>";
-
-                                foreach ($queries as $record_key => $record_query)
-                                {
-                                    if ($GLOBALS['connect'] -> query("$record_query") === TRUE) 
-                                    {
-                                        echo "[$table] -> [$record_key] has been added.<br><br>";
-                                    }
-                                    else
-                                    {
-                                        echo "[$table] -> ERROR adding [$record_key]: " . $GLOBALS['connect'] -> connect_error . "<br>";
-                                    }
-                                }
-                            }
-                            // If add_test_data is FALSE, skips the adding of records.
-                        }
-                    }
+                    // If the table doesn't exist, then the data also doesn't exist, so we're adding it
+                    createTables($table, $options); 
                 }
             }
         }
         //Finished Making the Tables
-        $GLOBALS['first_run_is_done'] = TRUE;   // FIXME: Add a .ini File to register when the first run has been done
+        $GLOBALS['first_run_is_done'] = TRUE;   // TODO: Add a .ini File to register when the first run has been done
         echo "<br><br>The database tables have been made.<br>Redirecting...";
         
     }
 
+    function createTables($table, $options)
+    {
+        /*
+            TO: Create the Tables using the table schema 
+            The tables to be added can be found in the Associative Array $tables[] on Line 9
+            Syntax for adding the table schema and data can be observed in the $tables[] array
+
+            NOTE: Used $options since the Associative Array has the options to either select the table's SCHEMA or the table's DATA
+                        Also used the same variable in the foreach on checkTables()
+        */
+        foreach ($options as $option => $queries)   // The SCHEMA Queries or the DATA input Queries
+        {
+            // Running through the second level to get the schema
+            /* 
+                The 2nd Level in the $tables array is for the SCHEMA, so if it's not an array, it is passed as a query.
+                And if it is an array, it means it's the DATA array, so we call addDataToTables() function to add data to the tables
+            */
+            if (is_array($queries) == FALSE)   
+            {
+                // If it's not an array, then it's the database schema
+                echo "The table [$table] doesn't exist...<br>Creating it now...";
+                $schema_query = $queries;   // To show you that the query in this case is a schema query
+
+                //Creating Table
+                if ($GLOBALS['connect'] -> query("$schema_query") == TRUE) 
+                {
+                    echo "<br>Table [$table] has been created successfully.<br>";
+                }
+                else
+                {
+                    echo "<br>Error Making the table [$table]: " . $GLOBALS['connect'] -> connect_error . "<br>";
+                }
+            }
+            else
+            {
+                // Checking whether the user wants the test data (NOTE: Check the Configuration.php File)
+                if ($GLOBALS['add_test_data'] == TRUE || $table === "USERS")    // TO: Allow the admin to be added despite the system user wanting no test data (The Admin allows the system to be managed)
+                {
+                    // If it is an array, it should go deeper to add the data to the table
+                    addDataToTables($table, $queries);
+                }
+                // If add_test_data is FALSE, skips the adding of records.
+            }
+        }
+    }
+
+    function addDataToTables($table, $queries)
+    {
+        // TO: Add data to the Table
+        echo "<br><u>Adding Data to [$table]...</u><br>";
+
+        foreach ($queries as $record_key => $record_query)
+        {
+            if ($GLOBALS['connect'] -> query("$record_query") === TRUE) 
+            {
+                echo "[$table] -> [$record_key] has been added.<br><br>";
+            }
+            else
+            {
+                echo "[$table] -> ERROR adding [$record_key]: " . $GLOBALS['connect'] -> connect_error . "<br>";
+            }
+        }
+    }
     //Running the first time connection
     createDB();
 ?>
