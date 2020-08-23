6dec13d40234caa51b27ad368f070ac30b4b9aca
neodevelopment <68895654+neodevelopment@users.noreply.github.com>
2020-08-23 15:13:38 +0300

    Modified the Terminal (Incomplete)

--- Pages/Terminal/Terminal.php
+++ Pages/Terminal/Terminal.php
@@ -1,26 +1,51 @@
 <?php   // Includes 
     require "../../Includes/Configuration/Connection.php";
-    require "../../Includes/Code/PHP/Queue.php";
+    require "../../Includes/Code/PHP/Algorithms.php";
     include "../../Includes/Code/Page Formats/Head.php";
     include "../../Includes/Code/Page Formats/Header.php";
 
 
     // The array with the data
-    $questions = [
-        "0" => array(
+    $questions_array = [  // This is the Questions Array
+        // Questions divided into groups
+        "Location" => array
+        (
             "question"  =>  "Where do you want go?",
-            "options"   =>  getLocationsArray(),
+            "answers"   =>  getFromDatabase("Location")
         ),
+
+        // Whether they're just picking up
+        "Pick Up" => array
+        (
+            "question"  =>  "Are you coming to pick up something?",
+            "answers"   =>  array
+            (
+                "Yes",
+                "No",
+            ),
+        ),
+
+        // If they're handicapped
+        "Handicap" => array
+        (
+            "question"  =>  "Are you Handicapped?",
+            "answers"   =>  array
+            (
+                "Yes",
+                "No",
+            ),
+        ),
+
         // Should be the last question in the array
-        "1" => array(
+        "Spot" => array
+        (
             "question"  =>  "Would you like this spot?",
-            "options"   =>  array(  // NOTE: Made it into this nested array in order to allow the code to remain the same
-                array(
-                    "Yes",
-                    "No"
-                ) 
-            )
-        )
+            "answers"   =>  array
+            (
+                "Yes",
+                "No",
+            ),
+        ),
     ];
 ?>
 
@@ -34,93 +59,104 @@
     <!-- Proceed with the Rest of the Body Container -->
     <div class="container">
         <div class="question">
-            <h1><?php echo $GLOBALS['questions'][currentQuestion()]['question']; ?></h1>
+            <h1><?php echo currentQuestion(); ?></h1>
         </div>
         <div class="suggestion">
-            <!-- TODO: The Parking Spot Suggestion  -->
-            <!-- <em></em>
-            <h2><?php // echo printFirstAvailableParking() ?></h2> -->
+            <em>Image of tick or error goes here</em>
+            <h2>Suggestion Goes here</h2>
         </div>
-        <div class="options">
+        <div class="selection-box">
             <div id="back">
-                <a href="javascript:history.back()"><img src="Media/Images/Back.png" alt="Back"></a>
+                <a href="#"><img src="Media/Images/Back.png" alt="Back"></a>
             </div>
-            <div class="select">
-                <!-- Pick the question from the associative array -->
-                <?php $options = $GLOBALS['questions'][currentQuestion()]['options'];?>
-                <?php foreach($options as $options_arrays => $keys):?>
-                <?php foreach($keys as $key => $value):?>
-                <div class="option">
-                    <!-- TODO: Needs Cleaning -->
-                    <!-- Creating an exception for when the user chooses no -->
-                    <?php
-                        if ($value === "No"){   ?>
-                    <a href="javascript:history.back()"><?php echo $value; ?></a>
-                    <?php } else { ?>
-                    <a href="?<?php echo "question=" . (currentQuestion() + 1); ?>"><?php echo $value; ?></a>
-                    <!-- TODO: Add GET Values for each of the options chosen -->
-                    <?php } ?>
-                </div>
-                <?php endforeach; ?>
-                <?php endforeach; ?>
+            <div class="options">
+                <?php currentOptions(); ?>
             </div>
+
         </div>
     </div>
 </body>
 
-</html>
+<?php   // For the Page Questions_array and redirects
+    function currentQuestion()
+    {
+        // Outputs the current question according to the associative array and user input
+        if (!isset($_GET['question_group']))
+        {
+            $_GET['question_group'] = "Location";
 
-<?php   // For the Page Questions and redirects
-    function currentQuestion(){
-        if (isset($_GET['question'])){
-            switch ($_GET['question']){
-                case 0:
-                    return 0;
+        }
+        return $GLOBALS['questions_array'][$_GET['question_group']]['question']; 
+    }
+
+    function currentOptions()
+    {
+        /* 
+            Gives an array of answers for the given question
+            Then outputs that array on the HTML
+        */ 
+
+        // Initializing Variables
+        $counter = 0;
+        $list = array();
+
+        foreach ($GLOBALS['questions_array'][$_GET['question_group']]['answers'] as $options => $option)
+        {
+            // Gets the array of options
+            $list[$counter] = $option;
+            $counter++;
+        }
+
+        // Displays the output
+        for($counter = 0; $counter < count($list); $counter++)
+        {
+            echo    "<div class='option'>".
+                        "<a href='#'>".
+                            $list[$counter].
+                        "</a>".
+                    "</div>";
+        }
+    }
+
+    function querySelector($question_group)
+    {
+        // Returns the query I need for the results
+        switch ($question_group)
+        {
+            case "Location":
+                return "SELECT DISTINCT P_LOCATION FROM PARKING";
                 break;
-                case 1:
-                    return 1;
+            default:
+                echo '  <script type="text/JavaScript">  
+                            alert("No valid query."); 
+                        </script>';
                 break;
-                default:
-                    return 404;
-                break;
+        }
+    }
+
+    function getFromDatabase($question_group)
+    {
+        // Gets the data from the database and converts it into a format the webpage can work with 
+        $query = querySelector($question_group);
+        $fetched = mysqli_query($GLOBALS['connect'], $query);
+        $arrays = mysqli_fetch_all($fetched, MYSQLI_ASSOC);
+        
+        // Check the type of answers we need
+        if ($question_group === "Location")
+        {
+            for ($counter = 0; $counter < count($arrays); $counter++)
+            {
+                $result[$counter] = $arrays[$counter]["P_LOCATION"];
             }
         }
-        // If it is unset, start with the first question
-        $_GET['question'] = 0;
-        return 0;
-    }
-
-    // TO: Get the Locations from the database
-    function getLocationsArray(){
-        // Querying the values from the database
-        $query = "SELECT DISTINCT P_LOCATION FROM PARKING";
-        $fetched = mysqli_query($GLOBALS['connect'], $query);
-        $result = mysqli_fetch_all($fetched, MYSQLI_ASSOC);
-
-        // Returning the Array
+        
         return $result;
-    }
-
-    function getAvailableParking($location){
-        // Querying the values from the database
-        $query = "SELECT P_ID FROM PARKING WHERE P_LOCATION = '$location' AND P_STATUS = 'Free'";
-        $fetched = mysqli_query($GLOBALS['connect'], $query);
-        $result = mysqli_fetch_all($fetched, MYSQLI_ASSOC);
-
-        // Returning the Array
-        return $result;
-    }
-
-    function printFirstAvailableParking(){
-        if (!isset($_GET['question']) || $_GET['question'] != 0)
-        {
-            return "Parking Spot ". (getAvailableParking("Mall")['0']['P_ID']) ." is available";
-        }
     }
 
     // TODO: Edit this out later
     // echo "<pre>";
-    // print_r (getAvailableParking("Mall"));
-    // echo array_keys(getAvailableParking("Mall"));
+    // print_r (getFromDatabase("Location"));
+    // print_r ($questions_array["Location"]["answers"]);
+    // print_r (getFromDatabase("Location"));
     // echo "</pre>";
 ?>
