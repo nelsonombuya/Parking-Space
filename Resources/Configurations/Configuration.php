d13e9b11462136dc56777a88d6d01bb6bc99cc27
neodevelopment <68895654+neodevelopment@users.noreply.github.com>
2020-09-06 21:46:21 +0300

    Refactor : Reduce Configuration.php
    > Did this to simplify the code
    
    - Comments on Configuration.php
    - Variables on Confuguration.php

---
+++ Resources/Configurations/Configuration.php
@@ -0,0 +1,11 @@
+<?php
+    //Settings for connecting to the database
+    //I've defined them as constants so that I can use them throughout other scripts
+    define ('server', "localhost");  
+    define ('server_user', "root");
+    define ('server_password', "1234");
+    define ('server_db', "Car_Parking_System");
+
+    // Setting Default Time Zone
+    date_default_timezone_set('Africa/Nairobi');
+?>
