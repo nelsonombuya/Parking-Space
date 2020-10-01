f24729b7273854b6a70dad087a800d95705ce3cc
neodevelopment <68895654+neodevelopment@users.noreply.github.com>
2020-10-01 23:39:01 +0300

    Refactor : Modify Session Script
    > Made some few additions to the Session script
    > Changed some function names on to better suit naming conventions
    
    + Session.php
    ~ Session.php

--- Resources/Scripts/Session.php
+++ Resources/Scripts/Session.php
@@ -1,8 +1,29 @@
 <?php
-    // Driver Details
+    // Session
+    // --------
+    // Starting the session if there is none
+    if (session_status() == PHP_SESSION_NONE){
+        session_start();
+    }
+
+    // Function for logging out
+    function logout(){
+        session_unset();
+        session_destroy();
+        header("Location: ../../Index.php") or die();
+    }
+
+    // User Details
     // ---------------
+    // Function for outputting the current username
+    function printCurrentUsername(){
+        if (isser($_SESSION['username'])){
+            return $_SESSION['username'];
+        }
+    }
+    
     // Function for outputting the current driver number
-    function driverNumber(){
+    function printCurrentDriverNumber(){
         $query =    "SELECT DRIVER_ID FROM DRIVERS 
                     ORDER BY DRIVER_ID DESC
                     LIMIT 1";
