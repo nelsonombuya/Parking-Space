e1f1bbc9e1bc920aad97393860b7f7a8a1b0355b
neodevelopment <68895654+neodevelopment@users.noreply.github.com>
2020-10-08 18:40:32 +0300

    Feature : Login to system (Refactor)
    > Added login with email and username
    > Added password hashing for more security
    > Added some few notes
    
    + Login.inc.php

--- Other Versions/Beta/Pages/Login/PHP/Login.inc.php
+++ Other Versions/Beta/Pages/Login/PHP/Login.inc.php
@@ -1,6 +1,6 @@
 <?php
     // Required Files
-    require $_SERVER['DOCUMENT_ROOT'] . "/Resources/Scripts/Includes.php"; 
+    require $_SERVER['DOCUMENT_ROOT'] . "/Resources/Scripts/Includes.php";
 
     // Checking for user input
     if (isset($_POST['login_username']) && isset($_POST['login_password']))
@@ -24,57 +24,86 @@
     // Function for logging in
     function login($username, $password)
     {
-        //The Query for checking for the user's details in the database
-        $query  =   "SELECT USERNAME, EMAIL, PASS 
-                    FROM USERS 
-                    WHERE USERNAME = ?
-                    OR EMAIL = ?";
+        // Checking for connection to DB
+        $connection_status = checkConnection();
 
-        // Using Prepared SQL statements for security
-        $stmt = mysqli_stmt_init(connectToDatabase());
-
-        if (!mysqli_stmt_prepare($stmt, $query))
+        if ($connection_status !== TRUE)
         {
             // If there's a connection error
-            header("Location: ../Login.php?error=sql_error") or die();
+            header("Location: ../Login.php?error=$connection_status") or die();
         } 
         else 
         {
-            // Executing the Query
-            mysqli_stmt_bind_param($stmt, "ss", $username, $username);
-            mysqli_stmt_execute($stmt);
-            $result = mysqli_stmt_get_result($stmt);
+            // Connecting to the database
+            $conn = connectToDatabase();
 
-            // If the user exists
-            if ($row = mysqli_fetch_assoc($result))
+            //The query for checking for the user's details in the database
+            $query  =   "SELECT USERNAME, EMAIL, PASS
+                        FROM USERS 
+                        WHERE USERNAME LIKE ?
+                        OR EMAIL LIKE ?";
+
+            // Preparing and executing the query
+            if (!$stmt =  $conn->stmt_init())
             {
-                // Checking Password
-                $password_check = password_verify($password, $row['PASS']);
-                if ($password_check === FALSE)
-                {
-                    // If the password is wrong
-                    header("Location: ../Login.php?error=wrong_pass") or die();
-                }
-                else if ($password_check === TRUE)
-                {
-                    // If the password is correct, the user is logged in successfully
-                    unset($_POST);
-                    session_start();
-                    $_SESSION['username'] = $row['USERNAME'];
+                // Error initializing the SQL Statement
+                header("Location: ../Login.php?error=sql_init") or die();
+            }
 
-                    // Redirect to Account Management Page
-                    header("Location: ../../Management/Account.php") or die();
+            else if (!$stmt = $conn->prepare($query))
+            {
+                // Error preparing the SQL Statement
+                header("Location: ../Login.php?error=sql_prepare") or die();
+            }
+            
+            else if (!$stmt->bind_param("ss", $username, $username))
+            {
+                // Error binding parameters
+                header("Location: ../Login.php?error=sql_bind") or die();
+            }
+            
+            else if (!$stmt->execute())
+            {
+                // Execute query
+                header("Location: ../Login.php?error=sql_execute") or die();
+            }
+            
+            else
+            {
+                // Getting the results of the query
+                $fetched = $stmt->get_result();
+                $result = mysqli_fetch_all($fetched, MYSQLI_ASSOC);
+
+                if (is_bool($result))
+                {    
+                    // The username or e-mail isn't in the database
+                    header("Location: ../Login.php?error=login_user") or die();
                 }
                 else 
                 {
-                    // If the password is wrong but due to a different error
-                    header("Location: ../Login.php?error=wrong_pass") or die();
+                    // Username of E-mail exists, checking Password
+                    $password_check = password_verify($password, $result['0']['PASS']);
+                    if ($password_check === FALSE)
+                    {
+                        // If the password is wrong
+                        header("Location: ../Login.php?error=login_pass") or die();
+                    }
+                    else if ($password_check === TRUE)
+                    {
+                        // If the password is correct, the user is logged in successfully
+                        unset($_POST);
+                        session_start();
+                        $_SESSION['username'] = $row['USERNAME'];
+
+                        // Redirect to Account Management Page
+                        header("Location: ../../Management/Account.php") or die();
+                    }
+                    else 
+                    {
+                        // If the password is wrong but due to a different error
+                        header("Location: ../Login.php?error=login_pass_") or die();
+                    }
                 }
-            } 
-            else 
-            {
-                // If the username doesn't exist in the database
-                header("Location: ../Login.php?error=user_dne") or die();
             }
         }
     }
--- Resources/Documents/ToDo.md
+++ Resources/Documents/ToDo.md
@@ -5,5 +5,12 @@
 2. Display Log when error occurs during first time setup
 3. Remember to organize general program security
 4. Add Guest Session
+5. Using Prepared Statements
+    - Also in the querying function
+    - During First Time Setup
+6. Update SQL Script with fix
+7. Change DB Status Error codes
+    - Minimize number of reconnects
+8. Fix Password Hashing on setup
 
 ## Current Commit Message
