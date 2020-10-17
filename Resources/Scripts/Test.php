84f3a055d806364231165edd29d666470f30475e
neodevelopment <68895654+neodevelopment@users.noreply.github.com>
2020-10-17 19:50:54 +0300

    Test : Test Jelix> Test for Jelix Ini Writer

--- Resources/Scripts/Test.php
+++ Resources/Scripts/Test.php
@@ -5,6 +5,7 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . "/Resources/Scripts/Test/Timer.inc.php"; 
 $stopwatch = new Stopwatch; $stopwatch->start();
+require_once $_SERVER['DOCUMENT_ROOT'] . "/Resources/Scripts/Includes.php";
 echo ("\n------------------------------------------------------------------------------------\n");
 /*----------------------------------------------------------------------------------------------*/ 
                                             /* -TEST- */
@@ -14,4 +15,20 @@ echo ("\n-----------------------------------------------------------------------
 /*----------------------------------------------------------------------------------------------*/
 ###################################################################################################
 echo ("\n------------------------------------------------------------------------------------\n");
+/*----------------------------------------------------------------------------------------------*/
+                                            /* PREVIOUS */
+                                        /* JELIX INI PARSER */
+    // print_r($before = parse_ini_file(
+    //     $_SERVER['DOCUMENT_ROOT'] . "/Resources/Settings/Settings.ini", TRUE));
+
+    // $ini = new \Jelix\IniFile\IniModifier(
+    //     $_SERVER['DOCUMENT_ROOT'] . "/Resources/Settings/Settings.ini");
+
+    //     // setting a parameter.  (section_name is optional)
+    // $ini->setValue('enabled', 'true', 'charges');
+    // $ini->save();
+
+    // print_r($after = parse_ini_file(
+    //     $_SERVER['DOCUMENT_ROOT'] . "/Resources/Settings/Settings.ini", TRUE));
+##################################################################################################
 $stopwatch->stop(); $stopwatch->print_time(); echo "</pre>"; ?>
