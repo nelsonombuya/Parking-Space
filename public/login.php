619659aac30b1df884c4db2e4b1b2c51c6951c1f
neodevelopment <68895654+neodevelopment@users.noreply.github.com>
2020-11-07 17:04:50 +0300

    Feature : (New Design) [WIP] > Added login.php

--- public/login.php
+++ public/login.php
@@ -17,79 +17,125 @@
     // If the user is already logged in, redirect to account settings
     if (isset($_SESSION['username'])) header("Location: " . HEADER_ROOT . "dashboard.php") or die();
 ?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Login Page</title>
-    <!-- Validation Javascript Script -->
-    <script type="text/javascript" src="js/login.validation.js"></script>
+    <title>Login</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Monsterrat Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 
-    <!-- Poppins Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
+    <!--Icon for Title-->
+    <link rel="icon" href="images/jeep.png" type="image/icon type">
 
-    <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="css/login.css" />
-
-    <!-- Javascript (From Fontawesome) [Has the Username And Password Icons] -->
-    <script src="https://kit.fontawesome.com/a81368914c.js"></script>
-
-    <!-- Page Metadata -->
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
 </head>
 
 <body>
-    <!-- The Background Wave Layer -->
-    <img class="wave" src="images/wave.png" />
 
-    <!-- The Page Content Container -->
-    <div class="container">
-        <!-- Image on top of the wave layer -->
-        <div class="img">
-            <img src="images/authentication.svg" />
-        </div>
-        <!--The Login Box-->
-        <div class="login-content">
-            <form name="login_form" onsubmit="return login_validation();" action="inc/login.inc.php"
-                method="POST">
-                <img src="images/avatar.svg" />
-                <h2 class="title">Account</h2>
+    <header>
 
-                <!-- Username or Email -->
-                <div class="input-div username">
-                    <div class="i">
-                        <i class="fas fa-user"></i>
-                    </div>
-                    <div class="div">
-                        <h5>Username/Email</h5>
-                        <input type="text" name="login_username" class="input" />
+        <!--Navbar Section-->
+
+        <nav class="navbar navbar-expand-md navbar-dark">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">
+                    <img src="images/lightjeep.png" width="25" height="25" alt="logo" class="animate__bounce ">
+                </a>
+                <a class="navbar-brand" href="index.php">Parking Space</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ml-auto">
+                        <a class="nav-item nav-link" href="index.php">Home </a>
+                        <a class="nav-item nav-link" href="index.php#about">About</a>
+                        <a class="nav-item nav-link" href="index.php#FAQ">FAQ</a>
+                        <a class="nav-item nav-link" href="index.php#Contacts">Contacts</a>
                     </div>
                 </div>
 
-                <!-- Password -->
-                <div class="input-div password">
-                    <div class="i">
-                        <i class="fas fa-lock"></i>
-                    </div>
-                    <div class="div">
-                        <h5>Password</h5>
-                        <input type="password" name="login_password" class="input" />
+            </div>
+        </nav>
+    </header>
+
+
+    <!--Login Section-->
+    <div class="container-fluid px-0 py-4 mt-4 text-dark ">
+        <div class="row">
+            <div class="col-md-12 col-md-offset-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="text-center">
+                            <a class="navbar-brand" href="#">
+                                <img src="images/account.png" width="50" height="50" alt="avatar">
+                            </a>
+                            <h3><i class="fa fa-lock fa-4x"></i></h3>
+                            <h2 class="text-center">Account Login</h2>
+                            <p>Please fill in the required details.</p>
+                            <div class="panel-body">
+
+                                <form id="register-form" name="login_form" role="form" autocomplete="off" class="form" method="post">
+
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <span class="glyphicon glyphicon-envelope"></span>
+                                            <input id="email" name="login_username" placeholder="Username/Email * "
+                                                class="form-control " type="email">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <input id="password" name="login_password" placeholder="Password *"
+                                                class="form-control " type="password">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <p class="text-center"><a href="password-recovery.php" id="signup">Forgot
+                                                password?</a></p>
+                                    </div>
+                                    <input name="recover-submit" class="btn btn-lg btn-dark btn-block" value="Login"
+                                        type="submit">
+                            </div>
+                            <div class="col-md-12 ">
+                                <div class="login-or">
+                                    <hr class="hr-or">
+                                    <span class="span-or">or</span>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <p class="text-center">Don't have account? <a href="#signup" id="signup.php">Sign up
+                                        here</a></p>
+                            </div>
+
+                            <input type="hidden" class="hide" name="token" id="token" value="">
+                            </form>
+
+                        </div>
                     </div>
                 </div>
-                <a href="#">Forgot Password?</a>
-                <!-- TODO: Come back here to link to the password recovery page -->
-                <input type="submit" class="btn" value="Sign In" />
-                <a href="index.php"><button type="button" class="btn" value="Guest">Continue as Guest</button></a>
-            </form>
+            </div>
         </div>
     </div>
+    </div>
 
-    <!-- Transitions Javascript-->
-    <script type="text/javascript" src="js/login.transitions.js"></script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
