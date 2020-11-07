cfa7ae3af1c5419953f34bc6eedc1e35107f343c
neodevelopment <68895654+neodevelopment@users.noreply.github.com>
2020-11-07 14:11:35 +0300

    Feature : (New Design) [WIP] Initial Commit > Got Benji's files for the latest website design

---
+++ archive/gamma/home.php
@@ -0,0 +1,126 @@
+<?php   // Includes 
+    require "Includes/Configuration/Connection.php";
+    require "Includes/Configuration/Session.php";
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>NeoDev Car Parking Systems</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <!--Icon for Title-->
+    <link rel="icon" href="jeep.png" type="image/icon type">
+
+
+
+  </head>
+  <body>
+
+
+        <header>
+
+        <!--Navbar Section-->
+
+             <nav class="navbar navbar-expand-md navbar-dark fixed-top ">
+                <div class="container">
+                     <a class="navbar-brand" href="#">
+                         <img src="jeep.png" width="25" height="25"  alt="logo" class="animate__bounce ">
+                     </a>
+                     <a class="navbar-brand" href="#">NeoDev</a>
+                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                     </button>
+                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                      <div class="navbar-nav ml-auto">
+                        <a class="nav-item nav-link" href="#">Home </a>
+                        <a class="nav-item nav-link" href="#about">About</a>
+                        <a class="nav-item nav-link" href="#FAQ">FAQ</a>
+                        <a class="nav-item nav-link" href="#contacts">Contacts</a>
+                      </div>
+                     </div>
+
+                 </div>
+             </nav>
+        </header>
+        
+
+
+        <main>
+            <!--Main Menu-->
+
+            <!--Section B-->
+
+
+            <!--About Section-->
+
+
+
+            <!--Contacts-->
+
+
+
+            <!--Footer-->
+
+
+<div class="row text-white text-center bg-dark pt-4">
+        <div class="col-sm-12">
+
+            <p class="font-weight-light">
+              <ul class="list-unstyled" >
+                  <li>NEODEV CAR PARKING SYSTEM</li>
+                  <li>Digital parking assitance </li>
+                  <li>Kabarak University</li>
+                  <li>PRIVATE BAG, NAKURU-KENYA</li>
+                  <li>Email: info@kabarakuniversity.ac.ke</li>
+                  <li>Tel: 0725 088 244 / 0758 466 254 </li>
+                  <li>
+                     <a class="navbar-brand" href="#">
+                         <img src="jeep.png" width="25" height="25"  alt="logo">
+                     </a>
+                  </li>
+              </ul>
+            </p>
+        </div>
+        <div class="container">
+          <ul class="list-inline">
+            <li class="list-inline-item"><a href="https://www.facebook.com/theserenityplacerehab/"><img src="/img/fb.png" width="25"></a></li>
+            <li class="list-inline-item"><a href="#"><img src="/img/twitter.png" width="27"></a></li>
+            <li class="list-inline-item"><a href="#"><img src="/img/insta.png" width="25"></a></li>
+            <li class="list-inline-item"><a href="#"><img src="/img/gmail.png" width="25"></a></li>
+          </ul>
+        </div>
+        <div class="container">
+          <div class="footer text-secondary">&#169;2020 COPYRIGHT - Nelson, Benjie, Vic.  </div>
+        </div>
+        
+      </div>
+
+
+
+ 
+        </main>
+
+
+
+
+
+        <footer>
+
+                        
+        </footer>
+
+
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
