d60aa3b153022b5f3971b8f83daf508d28902cce
neodevelopment <68895654+neodevelopment@users.noreply.github.com>
2020-11-08 22:56:54 +0300

    Chore : Add new design pages > Added newly designed pages (with reports and dashboard) to the project files

---
+++ archive/Team Project Front End/dashboard.php
@@ -0,0 +1,170 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Dashboard</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!--Icon for Title-->
+    <link rel="icon" href="img/jeep.png" type="image/icon type">
+
+  </head>
+  <body>
+
+  <header>
+
+<!--Navbar Section-->
+
+     <nav class="navbar navbar-expand-md navbar-dark">
+        <div class="container">
+             <a class="navbar-brand" href="index.php">
+                 <img src="img/jeep.png" width="25" height="25"  alt="logo">
+             </a>
+             <a class="navbar-brand" href="index.php">NeoDev</a>
+             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+             </button>
+             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav ml-auto">
+                <a class="nav-item nav-link" href="index.php">Home </a>
+                <a class="nav-item nav-link" href="#about">About</a>
+                <a class="nav-item nav-link" href="#FAQ">FAQ</a>
+                <a class="nav-item nav-link" href="#contacts">Contacts</a>
+              </div>
+             </div>
+
+         </div>
+     </nav>
+</header>
+
+
+  <!--Main Dashboard Section-->
+
+  <section class="testimonial pt-5" id="testimonial">
+    <div class="container">
+        <div class="row ">
+            <div class=" col-md-4 py-4 text-white text-center " >
+                <div class="rounded-left " id="left-side">
+                    <div class="card-body" >
+
+                     <img src="img/jeep.png" width="50" height="50"  alt="logo">
+
+                        <h2 class="pb-5 font-weight-bold" id="sign-head">
+                            ACCOUNT MANAGEMENT
+                        </h2>
+                        <p class="py-5 font-italic">Bringing tomorrow, today.</p>
+                    </div>
+                </div>
+             </div>
+             <div class="col-md-8 py-4 border-0">
+                 <h3 class="font-weight-bold pb-3">WELCOME ADMIN</h3>
+
+
+
+                 <div class="row">
+  <div class="col-4" >
+    <div class="list-group" id="list-tab" role="tablist">
+      <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Reports</a>
+      <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Another setting</a>
+      <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Another Setting</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
+    </div>
+  </div>
+  <div class="col-8">
+    <div class="tab-content" id="nav-tabContent">
+
+
+
+
+      <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+      
+      
+      
+      <ul class="list-group ">
+  <li class="list-group-item "><a href="#">Option1</a></li>
+  <li class="list-group-item"><a href="#">Option2</a></li>
+  <li class="list-group-item"><a href="#">Option3</a></li>
+  <li class="list-group-item"><a href="#">Option4</a></li>
+  <li class="list-group-item"><a href="#">Option5</a></li>
+</ul>
+      
+      
+      </div>
+
+
+      <div class="tab-pane fade " id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+      
+      
+      <ul class="list-group ">
+  <li class="list-group-item"><a href="#">Option1</a></li>
+  <li class="list-group-item"><a href="#">Option2</a></li>
+  <li class="list-group-item"><a href="#">Option3</a></li>
+  <li class="list-group-item"><a href="#">Option4</a></li>
+  <li class="list-group-item"><a href="#">Option5</a></li>
+</ul>
+      
+      
+      </div>
+      <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">
+      
+      
+      <ul class="list-group">
+  <li class="list-group-item"><a href="#">Option1</a></li>
+  <li class="list-group-item"><a href="#">Option2</a></li>
+  <li class="list-group-item"><a href="#">Option3</a></li>
+  <li class="list-group-item"><a href="#">Option4</a></li>
+  <li class="list-group-item"><a href="#">Option5</a></li>
+</ul>
+      
+      
+      </div>
+      <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
+      
+      <ul class="list-group">
+  <li class="list-group-item"><a href="#">Option1</a></li>
+  <li class="list-group-item"><a href="#">Option2</a></li>
+  <li class="list-group-item"><a href="#">Option3</a></li>
+  <li class="list-group-item"><a href="#">Option4</a></li>
+  <li class="list-group-item"><a href="#">Option5</a></li>
+</ul>
+      
+      
+      
+      </div>
+    </div>
+  </div>
+</div>
+<div class="go-home py-2 font-italic">
+             <p>
+                 <a class="navbar-brand" href="index.php">
+                 <img src="img/back.png" width="30" height="30"  alt="logo">
+                 </a><small>Go back to <a href="index.php" id="signup">Homepage.</small> </a>
+             </p>
+             
+
+            </div>
+
+            </div>
+
+
+        </div> 
+    </div>
+</section>
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
